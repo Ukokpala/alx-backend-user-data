7326d741b97a7970ac9ecf13416260137e07fcea
ukokpala <okpalaukamaka480@gmail.com>
2023-08-10 22:03:55 +0000

    update app.ppy

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,6 +23,9 @@ if AUTH_TYPE == 'auth':
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
@@ -35,11 +38,14 @@ def before_request():
     if auth is None:
         pass
     else:
+        setattr(request, "current_user", auth.current_user(request))
         excluded_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, excluded_list):
-            if auth.authorization_header(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
@@ -70,10 +76,10 @@ def forbidden(error) -> str:
     """_summary_
 
     Args:
-        error (_type_): _description_
+            error (_type_): _description_
 
     Returns:
-        str: _description_
+            str: _description_
     """
     return jsonify({"error": "Forbidden"}), 403
 
